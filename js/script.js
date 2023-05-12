02a1a01649116ce47efe63a27dc4a230b3127c82
Thikhathali <thikhamataboge@gmail.com>
2023-05-12 12:02:23 +0200

    Updated script file

--- js/script.js
+++ js/script.js
@@ -7,17 +7,25 @@ let wResultEl = document.querySelector('.win');
 let lResultEl = document.querySelector('.loose');
 const btnNewGame = document.querySelector('button.new-game');
 
+function playSound() {
+    const audio = document.querySelector('.correct');
+
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
+
 function getComputerChoice(){
-    const choice = ["rock", "paper" , "scissors" ];
+    const choice = ["💎", "🧻" , "✂" ];
     const intRandomIndex = Math.floor(Math.random() * 3);
     
     return choice[intRandomIndex];
 }
 
 function playRound(playerSelection, gameSelection){    
-    if(playerSelection.toLowerCase() === "rock" && gameSelection === "paper" || 
-       gameSelection === "scissors" && playerSelection.toLowerCase() === "paper" ||
-       playerSelection.toLowerCase() === "scissors" && gameSelection === "rock"){
+    if(playerSelection.toLowerCase() === "💎" && gameSelection === "🧻" || 
+       gameSelection === "✂" && playerSelection.toLowerCase() === "🧻" ||
+       playerSelection.toLowerCase() === "✂" && gameSelection === "💎"){
         return `You lose! ${gameSelection} beats ${playerSelection.toLowerCase()}`
     } else if(playerSelection.toLowerCase() === gameSelection){
         return `Game drawn!`
@@ -32,13 +40,15 @@ let cpu_score = 0;
 function showResults(num){
     const newGame = document.createElement('div');
     if(num === 1) {
+        document.querySelector('.success').play();
         cpuSelectionEl.textContent = '🥇';
         boardEl.style.boxShadow = '1em 0 5em 8px rgb(0, 255, 98)';
         btnEls.forEach(button => button.innerHTML = '');
-        newGame.innerHTML = `<button class="new-game"> New Game? </button>`; 
+        newGame.innerHTML = `<button class="new-game" onClick="reset(this);"> New Game? </button>`; 
         cpuSelectionEl.appendChild(newGame);
     } 
     else if(num === 0) {
+        document.querySelector('.fail').play();
         cpuSelectionEl.textContent = '😞';
         btnEls.forEach(button => button.innerHTML = '');
         boardEl.style.boxShadow = '1em 0 5em 8px rgb(255, 0, 0)';
@@ -58,6 +68,7 @@ function game(){
     console.log(res);
 
     if(res.substring(4, 7) == "win"){
+        playSound();
         wResultEl.className = 'visible';
         playerScoreEl.innerHTML = `PLAYER: <br>${++player_score}`
     } else  if(res.substring(4, 8) == "lose"){
@@ -66,23 +77,17 @@ function game(){
     }
     console.log("S C O R E: " + "\n" + "PLAYER_1: " + player_score + "\n" + "CPU: " + cpu_score);
 
-    if(player_score === 5) {        
-        showResults(1);
-        console.log(`You won! by ${player_score} rounds`);
-    } else if(cpu_score === 5){
-        showResults(0);
-        console.log(`You lost! by ${cpu_score} rounds`);
-    }
+    if(player_score === 5) showResults(1);
+     else if(cpu_score === 5) showResults(0);  
 }
 
 function reset(){
     location.reload();
-    console.log('It Works!')
 }
 
 function resetResultElClass(){        
     setInterval(() => {
-         wResultEl.classList.replace('visible', 'win'); console.log('h')
+         wResultEl.classList.replace('visible', 'win');
         }, 2000);
     setInterval(() => { 
         lResultEl.classList.replace('visible', 'loose');
